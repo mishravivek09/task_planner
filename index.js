31a3ae1851b41731940fc9bfd8c55256548e8e3b
Vivek Mishra <100134407+mishravivek09@users.noreply.github.com>
2023-03-25 15:34:45 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,242 @@
+let user = JSON.parse(localStorage.getItem("user")) || [];
+document.querySelector("#signinPage").addEventListener("click", function () {
+    getUser();
+})
+if (user.length != 0) {
+    document.querySelector("#signinPage").innerHTML = `Hi, ${user.firstName}`;
+}
+function getUser() {
+    if (user.length == 0) {
+        window.location.href = "signup.html";
+    } else {
+        window.location.href = "index.html";
+    }
+}
+
+
+function getSignupData() {
+    event.preventDefault();
+    let form = document.querySelector("#signupForm");
+    let fname = form.fname.value;
+    let lname = form.lname.value;
+    let email = form.email.value;
+    let passwd = form.password.value;
+    let gender = form.gender.value;
+
+    let signupData = {
+        firstName: fname,
+        lastName: lname,
+        gender: gender,
+        email: email,
+        password: passwd
+    }
+    let data = JSON.stringify(signupData);
+    fetch("https://task-planner-zvg7.onrender.com:443/api/v1/user/register", {
+        method: "POST",
+        body: data,
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then((res) => {
+        return res.json();
+    }).then((res) => {
+        if (res.message == null) {
+            alert("Signup Successfull !");
+        } else {
+            alert(res.message);
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+    form.reset();
+}
+
+function verifyUser() {
+    event.preventDefault();
+    let form = document.querySelector("#loginForm");
+    let loginData = {
+        email: form.lemail.value,
+        password: form.lpassword.value
+    }
+    let data = JSON.stringify(loginData);
+    fetch("https://task-planner-zvg7.onrender.com:443/api/v1/user/validate", {
+        method: "POST",
+        body: data,
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then((res) => {
+        return res.json();
+    }).then((res) => {
+        if (res.message == null) {
+            alert("Login Successful !");
+            localStorage.setItem("user", JSON.stringify(res));
+            window.location.href = "index.html";
+        } else {
+            alert(res.message);
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+    form.reset();
+}
+if (user.length != 0) {
+    getAllTask(user);
+}
+document.querySelector("#allTask").addEventListener("click", function () {
+    getAllTask(user);
+})
+function getAllTask(user) {
+    if (user.length == 0) {
+        alert("Please login first !");
+    } else {
+        let url = `https://task-planner-zvg7.onrender.com:443/api/v1/task/all?userId=${user.userId}`;
+        fetch(url).then((res) => {
+            return res.json();
+        }).then((res) => {
+            showTasks(res);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+}
+document.querySelector("#pendingTask").addEventListener("click", function () {
+    getPendingTask(user);
+})
+function getPendingTask(user) {
+    if (user.length == 0) {
+        alert("Please login first !");
+    } else {
+        let url = `https://task-planner-zvg7.onrender.com:443/api/v1/task/pending?userId=${user.userId}`;
+        fetch(url).then((res) => {
+            return res.json();
+        }).then((res) => {
+            showTasks(res);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+}
+document.querySelector("#completedTask").addEventListener("click", function () {
+    getCompletedTask(user);
+})
+function getCompletedTask(user) {
+    if (user.length == 0) {
+        alert("Please login first !");
+    } else {
+        let url = `https://task-planner-zvg7.onrender.com:443/api/v1/task/completed?userId=${user.userId}`;
+        fetch(url).then((res) => {
+            return res.json();
+        }).then((res) => {
+            showTasks(res);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+}
+function showTasks(data) {
+    let container = document.querySelector("#container");
+    container.innerHTML = "";
+    data.forEach(element => {
+        let div = document.createElement("div");
+        let title = document.createElement("h2");
+        let desc = document.createElement("p");
+        let status = document.createElement("select");
+        let date = document.createElement("p");
+        let option1 = document.createElement("option");
+        let option2 = document.createElement("option");
+
+        if (element.taskStatus == "PENDING") {
+            option1.innerHTML = "Pending";
+            option2.innerHTML = "Completed";
+            option1.value = "PENDING";
+            option2.value = "COMPLETED"
+        } else {
+            option1.innerHTML = "Completed";
+            option2.innerHTML = "Pending";
+            option1.value = "COMPLETED";
+            option2.value = "PENDING"
+        }
+        status.append(option1, option2);
+        status.addEventListener("change", function () {
+            updateStatus(status.value, element.taskId);
+        })
+        title.innerHTML = element.title;
+        desc.innerHTML = element.description;
+        let dateTime = element.dateTime;
+        let dateObj = new Date(dateTime);
+        date.innerHTML = `Date : ${dateObj.toDateString()}`;
+
+        div.append(title, desc, date, status);
+
+        container.append(div);
+    });
+}
+function updateStatus(value, id) {
+    let updateValue = {
+        taskId: id,
+        taskStatus: value
+    }
+    let data = JSON.stringify(updateValue);
+    fetch("https://task-planner-zvg7.onrender.com:443/api/v1/task/update", {
+        method: "PUT",
+        body: data,
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then((res) => {
+        return res.json();
+    }).then((res) => {
+        if (res.message == null) {
+            alert("Status Updated !");
+            window.location.reload();
+        } else {
+            alert(res.message);
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+document.querySelector("#addTask").addEventListener("click",function(){
+    createTask(user);
+})
+function createTask(user) {
+    if (user.length == 0) {
+        alert("Please login first !");
+    } else {
+        let title = prompt("Title", "Test");
+        let desc = prompt("Description", "This is a demo");
+        if(title.length<3 || desc.length <3){
+            alert("Please enter valid data..");
+        }else{
+            let taskData = {
+                title: title,
+                description: desc,
+                userId:user.userId
+            }
+            let data = JSON.stringify(taskData);
+            addTask(data);
+        }
+    }
+}
+function addTask(data) {
+    let url = `https://task-planner-zvg7.onrender.com:443/api/v1/task/add`;
+    fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then((res) => {
+        return res.json();
+    }).then((res) => {
+        if (res.message == null) {
+            alert("Task added successfully !");
+            window.location.reload();
+        } else {
+            alert(res.message);
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+}
